8c9a9702fb6aca8a11deffe97bfd9b6bccc7e0e0
Cantelmidavide <d.cantelmi@apio.cc>
2023-05-03 16:28:53 +0200

    feat: added first skeleton of the simulation

--- main.py
+++ main.py
@@ -58,20 +58,52 @@ class Vegetob:
         self.density = density
 
 class Erbast:
-    def __init__(self, energy, lifetime, social_attitude):
-        self.energy = energy
-        self.lifetime = lifetime
-        self.age = 0
-        self.social_attitude = social_attitude
+    def __init__(self):
+        self.name = "Erbast"
+        self.nutrition_value = 2
+        self.move_range = 1
+        self.herd_size = 10
+        self.quantity = 10
 
-    def update_age(self):
-        self.age += 1
+    def move(self, grid):
+        i, j = self.find_location(grid)
+        neighbors = self.find_neighbors(grid, i, j, self.move_range)
+        if len(neighbors) > 0:
+            new_i, new_j = random.choice(neighbors)
+            grid[i][j].remove(self)
+            grid[new_i][new_j].append(self)
+
+    def find_location(self, grid):
+        for i in range(NUMCELLS):
+            for j in range(NUMCELLS):
+                if self in grid[i][j]:
+                    return i, j
+
+    def find_neighbors(self, grid, i, j, radius):
+        neighbors = []
+        for di in range(-radius, radius+1):
+            for dj in range(-radius, radius+1):
+                if abs(di) + abs(dj) <= radius and 0 <= i+di < NUMCELLS and 0 <= j+dj < NUMCELLS:
+                    if "ground" in grid[i+di][j+dj]:
+                        neighbors.append((i+di, j+dj))
+        return neighbors
+    
+    def graze(self, grid):
+        i, j = self.find_location(grid)
+        for di in range(-1, 2):
+            for dj in range(-1, 2):
+                if 0 <= i+di < NUMCELLS and 0 <= j+dj < NUMCELLS:
+                    for plant in grid[i+di][j+dj]:
+                        if isinstance(plant, Vegetob):
+                            self.quantity += plant.nutrition_value
+                            plant.quantity -= plant.nutrition_value
+                            if plant.quantity < 0:
+                                grid[i+di][j+dj].remove(plant)
 
 class Carviz:
     def __init__(self, energy, lifetime, social_attitude):
         self.energy = energy
         self.lifetime = lifetime
-
 
 grid = [[None for _ in range(NUMCELLS)] for _ in range(NUMCELLS)]
 
@@ -85,9 +117,28 @@ for i in range(NUMCELLS):
             else:
                 grid[i][j] = "water"
 
+# Define a function to find the neighboring cells of a given position
+def find_neighbors(position, move_range):
+    neighbors = []
+    for i in range(position[0]-move_range, position[0]+move_range+1):
+        for j in range(position[1]-move_range, position[1]+move_range+1):
+            if i >= 0 and i < NUMCELLS and j >= 0 and j < NUMCELLS and (i,j) != position:
+                neighbors.append((i,j))
+    return neighbors
+
 def update_grid():
-    # TODO: implement the logic for updating the grid based on the movement and behavior of the species
-    pass
+    new_grid = [[None for _ in range(NUMCELLS)] for _ in range(NUMCELLS)]
+    for i in range(NUMCELLS):
+        for j in range(NUMCELLS):
+            if grid[i][j] == "ground":
+                if "Vegetob" in grid[i][j]:
+                    new_vegetob = int(grid[i][j]["Vegetob"] * (1 + Vegetob().growth_rate))
+                    new_vegetob = min(new_vegetob, 9)
+                    new_grid[i][j] = {"Vegetob": new_vegetob}
+                elif "Erbast" in grid[i][j]:
+                    if random.random() < 0.5:
+                        neighbors = find_neighbors
+
 
 # Run the simulation for NUMDAYS days
 for day in range(NUMDAYS):
