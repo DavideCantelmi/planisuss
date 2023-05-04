59e104d133597aa020e0083e2cd901b6945a67be
Cantelmidavide <d.cantelmi@apio.cc>
2023-05-04 10:48:33 +0200

    code updated

--- main.py
+++ main.py
@@ -31,117 +31,115 @@ AGING_C = 1          # energy lost each month for Carviz
 GROWING = 1          # Vegetob density that grows per day.
 
 
-import random
-
-NUMCELLS = 10
-
-NUMDAYS = 100
-
-class Cell:
-    def __init__(self, x, y, terrain):
-        self.x = x
-        self.y = y
-        self.terrain = terrain
-        self.vegetob_density = 0
-        self.erbast_list = []
-        self.carviz_list = []
-        self.neighborhood = []
-
-    def add_erbast(self, erbast):
-        self.erbast_list.append(erbast)
-
-    def add_carviz(self, carviz):
-        self.carviz_list.append(carviz)
 
 class Vegetob:
     def __init__(self, density):
-        self.density = density
+        self.set_density(density)
+
+    def set_density(self, density):
+        if density < 0:
+            self.density = 0
+        elif density > 100:
+            self.density = 100
+        else:
+            self.density = density
+
+    def get_density(self):
+        return self.density
 
 class Erbast:
-    def __init__(self):
-        self.name = "Erbast"
-        self.nutrition_value = 2
-        self.move_range = 1
-        self.herd_size = 10
-        self.quantity = 10
-
-    def move(self, grid):
-        i, j = self.find_location(grid)
-        neighbors = self.find_neighbors(grid, i, j, self.move_range)
-        if len(neighbors) > 0:
-            new_i, new_j = random.choice(neighbors)
-            grid[i][j].remove(self)
-            grid[new_i][new_j].append(self)
-
-    def find_location(self, grid):
-        for i in range(NUMCELLS):
-            for j in range(NUMCELLS):
-                if self in grid[i][j]:
-                    return i, j
-
-    def find_neighbors(self, grid, i, j, radius):
-        neighbors = []
-        for di in range(-radius, radius+1):
-            for dj in range(-radius, radius+1):
-                if abs(di) + abs(dj) <= radius and 0 <= i+di < NUMCELLS and 0 <= j+dj < NUMCELLS:
-                    if "ground" in grid[i+di][j+dj]:
-                        neighbors.append((i+di, j+dj))
-        return neighbors
-    
-    def graze(self, grid):
-        i, j = self.find_location(grid)
-        for di in range(-1, 2):
-            for dj in range(-1, 2):
-                if 0 <= i+di < NUMCELLS and 0 <= j+dj < NUMCELLS:
-                    for plant in grid[i+di][j+dj]:
-                        if isinstance(plant, Vegetob):
-                            self.quantity += plant.nutrition_value
-                            plant.quantity -= plant.nutrition_value
-                            if plant.quantity < 0:
-                                grid[i+di][j+dj].remove(plant)
-
-class Carviz:
     def __init__(self, energy, lifetime, social_attitude):
         self.energy = energy
         self.lifetime = lifetime
+        self.age = 0
+        self.social_attitude = social_attitude
+        self.neighborhood = NEIGHBORHOOD
 
-grid = [[None for _ in range(NUMCELLS)] for _ in range(NUMCELLS)]
+    def get_energy(self):
+        return self.energy
 
-for i in range(NUMCELLS):
-    for j in range(NUMCELLS):
-        if i == 0 or j == 0 or i == NUMCELLS-1 or j == NUMCELLS-1:
-            grid[i][j] = "water"
-        else:
-            if random.random() < 0.5:
-                grid[i][j] = "ground"
+    def set_energy(self, energy):
+        self.energy = energy
+
+    def get_lifetime(self):
+        return self.lifetime
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+    def get_social_attitude(self):
+        return self.social_attitude
+
+    def get_neighborhood(self):
+        return self.neighborhood
+
+    def set_neighborhood(self, neighborhood):
+        self.neighborhood = neighborhood
+
+    def join_herd(self, other):
+        if isinstance(other, Erbast) and other in self.herd:
+            return "The other individual is already in the herd."
+
+        if isinstance(other, Erbast) and self.age >= 30 and other.age >= 30:
+            similarity = abs(self.social_attitude - other.social_attitude)
+            distance = ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
+
+            if distance <= NEIGHBORHOOD and similarity <= 0.1:
+                self.herd.append(other)
+                other.herd.append(self)
+                return "Individuals have joined the herd successfully."
+
             else:
-                grid[i][j] = "water"
+                return "The other individual is too far away or is not compatible with the herd."
 
-# Define a function to find the neighboring cells of a given position
-def find_neighbors(position, move_range):
-    neighbors = []
-    for i in range(position[0]-move_range, position[0]+move_range+1):
-        for j in range(position[1]-move_range, position[1]+move_range+1):
-            if i >= 0 and i < NUMCELLS and j >= 0 and j < NUMCELLS and (i,j) != position:
-                neighbors.append((i,j))
-    return neighbors
+        else:
+            return "The other individual cannot join the herd."
+    
+class Carviz:
+    NEIGHBORHOOD = 5
 
-def update_grid():
-    new_grid = [[None for _ in range(NUMCELLS)] for _ in range(NUMCELLS)]
-    for i in range(NUMCELLS):
-        for j in range(NUMCELLS):
-            if grid[i][j] == "ground":
-                if "Vegetob" in grid[i][j]:
-                    new_vegetob = int(grid[i][j]["Vegetob"] * (1 + Vegetob().growth_rate))
-                    new_vegetob = min(new_vegetob, 9)
-                    new_grid[i][j] = {"Vegetob": new_vegetob}
-                elif "Erbast" in grid[i][j]:
-                    if random.random() < 0.5:
-                        neighbors = find_neighbors
+    def __init__(self, energy, lifetime, social_attitude):
+        self.energy = energy
+        self.lifetime = lifetime
+        self.age = 0
+        self.social_attitude = social_attitude
 
+    def get_energy(self):
+        return self.energy
 
-# Run the simulation for NUMDAYS days
-for day in range(NUMDAYS):
-    update_grid()
+    def set_energy(self, energy):
+        self.energy = energy if energy >= 0 else 0
 
-# TODO: analyze the results of the simulation and plot any necessary graphs or charts
+    def get_lifetime(self):
+        return self.lifetime
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+    def get_social_attitude(self):
+        return self.social_attitude
+
+    def set_social_attitude(self, social_attitude):
+        self.social_attitude = social_attitude if 0 <= social_attitude <= 1 else 0 if social_attitude < 0 else 1
+
+    def hunt(self):
+        self.energy += 10
+
+    def move(self):
+        self.energy -= 1
+
+    def is_alive(self):
+        return self.energy > 0 and self.age < self.lifetime
+
+    def join_pride(self, neighbors):
+        num_pride = sum([1 for neighbor in neighbors if isinstance(neighbor, Carviz)])
+        pride_attitude = num_pride / self.NEIGHBORHOOD
+        if pride_attitude >= self.social_attitude:
+            return True
+        return False
